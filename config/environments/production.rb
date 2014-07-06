6a87ce854c65dd44fc04761de2f5cb6745423dfe
Jenny Herrera <jennyherrera.19@gmail.com>
2014-07-05 23:13:41 -0700

    add amazon s3 uploads from paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,14 @@ Rails.application.configure do
 
   #required for devise, set to host name
   config.action_mailer.default_url_options = { :host => 'http://pinteresting.herreraj.com' }
+
+  # sets paperclip to upload images on amazon s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
