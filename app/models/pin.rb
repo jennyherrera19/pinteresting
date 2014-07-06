77580668e2e0d968b9d95e084995aa66278bb2e1
Jenny Herrera <jennyherrera.19@gmail.com>
2014-07-05 22:06:24 -0700

    fixed imagemagick issue

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => {:medium => "300X300>", :thumb => "100X100>"}
+	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
