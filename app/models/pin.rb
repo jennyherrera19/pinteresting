b2c39f3d5fa7d632eb726f2395389213e5f6a045
Jenny Herrera <jennyherrera.19@gmail.com>
2014-07-07 00:37:43 -0700

    changed layout, added name for users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,5 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => {:medium => "300X300>", :thumb => "100X100>"}
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+	validates :description, presence: true
 end
